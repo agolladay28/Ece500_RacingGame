a5dfeab24873c60bb8ee09c59b928e3f2a6eaa91
Joe Maloney <maloney.jg2@gmail.com>
2026-02-27 14:32:23 -0500

    demo day hotfix, remove builds folder from git tracking

--- racing_game/Assets/scripts/MusicManager.cs
+++ racing_game/Assets/scripts/MusicManager.cs
@@ -1,10 +1,8 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MusicManager : MonoBehaviour
 {
     private static MusicManager instance;
-    private AudioSource audioSource;
 
     void Awake()
     {
@@ -16,28 +14,6 @@ public class MusicManager : MonoBehaviour
 
         instance = this;
         DontDestroyOnLoad(gameObject);
-
-        audioSource = GetComponent<AudioSource>();
-
-        if (!audioSource.isPlaying)
-            audioSource.Play();
-
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        if (scene.name == "Start Page" ||
-            scene.name == "Control" ||
-            scene.name == "Track Selection")
-            return;
-
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-        Destroy(gameObject);
-    }
-
-    void OnDestroy()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
+
