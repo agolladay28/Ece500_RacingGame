4c008fbf9059e3074e623ba09f802aded3e543a1
Joe Maloney <maloney.jg2@gmail.com>
2026-03-19 07:43:41 -0400

    wheel scripting

---
+++ racing_game/Assets/scripts/SteeringWheel.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using Oculus.Interaction;
+public class SteeringWheel : MonoBehaviour
+{
+    public float angle;
+    private bool grabbed = false;
+    private Vector3 starting_angle;
+    public Grabbable grabbable;
+
+    void OnEnable()
+    {
+        grabbable.WhenPointerEventRaised += HandleEvent;
+    }
+
+    void OnDisable()
+    {
+        grabbable.WhenPointerEventRaised -= HandleEvent;
+    }
+    void Start()
+    {
+        starting_angle = transform.localEulerAngles;
+    }
+    private void HandleEvent(PointerEvent evt)
+    {
+        if (evt.Type == PointerEventType.Select)
+        {
+            Debug.Log("Grabbed");
+            grabbed = true;
+        }
+        else if (evt.Type == PointerEventType.Unselect)
+        {
+            Debug.Log("Released");
+            grabbed = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        angle = transform.localEulerAngles.y;
+        if (!grabbed)
+        {
+            transform.localEulerAngles = starting_angle;
+        }
+        //  Debug.Log("Relative Y Rotation: " + angle);
+    }
+
+}
